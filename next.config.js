4b83eab490d03127a892db65835ee4c0d9974715
Seif <Seifboukherouf@gmail.com>
2021-11-26 17:46:51 +0100

    main

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "image.tmdb.org",]
   },
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
+  }
 };
