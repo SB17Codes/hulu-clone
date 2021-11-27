9e705a88214f9b8d76dab8d805e315cca3275a99
Seif <Seifboukherouf@gmail.com>
2021-11-27 14:04:12 +0100

    main

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,14 +1,16 @@
 import Image from "next/dist/client/image";
-import { ThumbUpIcon } from "@heroicons/react/outline";
+import { ThumbUpIcon, PlayIcon } from "@heroicons/react/outline";
+import {useRouter} from 'next/router'
 
 const Thumbnail = ({ result }) => {
+
+  const router = useRouter()
+
   const BASE_URL = "https://image.tmdb.org/t/p/original";
   return (
-    <div
-      className=" p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50  "
-    >
+    <div onClick={() => router.push(`/movie/${result.id}`)} className=" relative p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 ">
       <Image
-        className="rounded"
+        className="rounded group-hover:opacity-80"
         layout="responsive"
         src={
           `${BASE_URL}${result.backdrop_path}` ||
@@ -19,7 +21,7 @@ const Thumbnail = ({ result }) => {
       />
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
-        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+        <h2  className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
           {result.title || result.original_name}
         </h2>
         <p className="flex items-center opacity-0 group-hover:opacity-100 ">
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Nav from "../components/Nav";
 import Results from "../components/Results";
 import requests from "../utils/requests";
 
-export default function Home({results}) {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -16,7 +16,7 @@ export default function Home({results}) {
       {/*Navbar*/}
       <Nav />
       {/*Results*/}
-      <Results results={results}/>
+      <Results results={results} />
     </div>
   );
 }
@@ -26,13 +26,14 @@ export async function getServerSideProps(context) {
 
   const request = await fetch(
     `https://api.themoviedb.org/3${
-      requests[genre]?.url || requests.fetchTrending.url}
+      requests[genre]?.url || requests.fetchTrending.url
+    }
       `
   ).then((res) => res.json());
 
   return {
     props: {
       results: request.results,
-    }
-  }
+    },
+  };
 }
